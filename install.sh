ca47684aa8b8e6fd6f9eb5a2fc3004687afc448f
David Love <davidklove@pm.me>
2020-12-23 11:13:50 -0700

    Major improvements to install.sh
    1. Can execute from anywhere
    2. Make ~/.config if it doesn't exist
    3. Use a single loop for all files
    4. Add options to delete or backup existing config files

--- install.sh
+++ install.sh
@@ -2,52 +2,58 @@
 
 # Install links to the dotfiles directory for all tracked files
 
-for x in .*; do
-    if [ $x = '.' ] \
-    || [ $x = '..' ] \
-    || [ $x = '.git' ] \
-    || [ $x = '.gitignore' ] \
-    || [ $x = '.gitmodules' ] \
-    || [ $x = 'README' ] \
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P )"
+
+if [ ! -d $HOME/.config ]; then
+    CMD="mkdir $HOME/.config"
+    echo "$CMD"
+    eval "$CMD"
+fi
+
+for x in $DIR/.* $DIR/config/*; do
+    basex=$(realpath "$x" --relative-to "$DIR")
+
+    if [ $basex = '.' ] \
+    || [ $basex = '..' ] \
+    || [ $basex = '.git' ] \
+    || [ $basex = '.gitignore' ] \
+    || [ $basex = '.gitmodules' ] \
+    || [ $basex = 'README' ] \
     ; then
         continue
     fi
-    SOURCE=$PWD/$x
-    DEST=$HOME/$x
+
+    SOURCE=$x
+    DEST=$HOME/$(echo $basex | sed 's:^config/:.config/:')
     if [ -e $DEST ] ; then
         if [ -L $DEST ] && [ "$(realpath $SOURCE)" == "$(readlink $DEST)" ] ; then
-            echo "${SOURCE} already installed"
+            echo "${SOURCE} already installed to ${DEST}"
             continue
         else
-            read -p "$DEST already exists, delete it? y/[n] > " RESPONSE
-            if [ "$RESPONSE" == "y" ]; then
-                rm -rf $DEST
+            if [ "$1" == "-d" ]; then
+                RESPONSE="d"
+            elif [ "$1" == '-b' ]; then
+                RESPONSE="b"
+            elif [ "$1" == '-n' ]; then
+                RESPONSE="n"
             else
+                read -p "$DEST already exists, [d]elete it, [b]ackup, or [n]o change (default) > " RESPONSE
+            fi
+            if [ "$RESPONSE" == "d" ]; then
+                CMD="rm -rf $DEST"
+                echo "$CMD"
+                eval "$CMD"
+            elif [ "$RESPONSE" == "b" ]; then
+                CMD="mv -f $DEST $DEST.bak"
+                echo "$CMD"
+                eval "$CMD"
+            else
+                echo "Skipping $DEST"
                 continue
             fi
         fi
     fi
-    cmd="ln -s $SOURCE $DEST"
+    CMD="ln -s $SOURCE $DEST"
     echo "Installing link: $SOURCE -> $DEST"
-    eval "$cmd"
-done
-for x in config/*; do
-    SOURCE=$PWD/$x
-    DEST=$HOME/.$x
-    if [ -e $DEST ] ; then
-        if [ -L $DEST ] && [ "$(realpath $SOURCE)" == "$(readlink $DEST)" ] ; then
-            echo "${SOURCE} already installed"
-            continue
-        else
-            read -p "$DEST already exists, delete it? y/[n] > " RESPONSE
-            if [ "$RESPONSE" == "y" ]; then
-                rm -rf $DEST
-            else
-                continue
-            fi
-        fi
-    fi
-    cmd="ln -s $SOURCE $DEST"
-    echo "Installing link: $SOURCE -> $DEST"
-    eval "$cmd"
+    eval "$CMD"
 done
