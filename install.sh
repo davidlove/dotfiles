a0a59b817780c2b9f781afd5f52bdaeef1583e9f
David Love <davidklove@pm.me>
2021-07-12 11:27:35 -0400

    Fix install.sh to work with spaces in directories

--- install.sh
+++ install.sh
@@ -10,50 +10,53 @@ if [ ! -d $HOME/.config ]; then
     eval "$CMD"
 fi
 
-for x in $DIR/.* $DIR/config/*; do
+for x in "$DIR"/.* "$DIR"/config/*; do
     basex=$(realpath "$x" --relative-to "$DIR")
 
-    if [ $basex = '.' ] \
-    || [ $basex = '..' ] \
-    || [ $basex = '.git' ] \
-    || [ $basex = '.gitignore' ] \
-    || [ $basex = '.gitmodules' ] \
-    || [ $basex = 'README' ] \
+    if [ "$basex" = '.' ] \
+    || [ "$basex" = '..' ] \
+    || [ "$basex" = '.git' ] \
+    || [ "$basex" = '.gitignore' ] \
+    || [ "$basex" = '.gitmodules' ] \
+    || [ "$basex" = 'README' ] \
     ; then
         continue
     fi
 
-    SOURCE=$x
-    DEST=$HOME/$(echo $basex | sed 's:^config/:.config/:')
-    if [ -e $DEST ] ; then
-        if [ -L $DEST ] && [ "$(realpath $SOURCE)" == "$(readlink $DEST)" ] ; then
-            echo "${SOURCE} already installed to ${DEST}"
-            continue
-        else
-            if [ "$1" == "-d" ]; then
-                RESPONSE="d"
-            elif [ "$1" == '-b' ]; then
-                RESPONSE="b"
-            elif [ "$1" == '-n' ]; then
-                RESPONSE="n"
-            else
-                read -p "$DEST already exists, [d]elete it, [b]ackup, or [n]o change (default) > " RESPONSE
-            fi
-            if [ "$RESPONSE" == "d" ]; then
-                CMD="rm -rf $DEST"
-                echo "$CMD"
-                eval "$CMD"
-            elif [ "$RESPONSE" == "b" ]; then
-                CMD="mv -f $DEST $DEST.bak"
-                echo "$CMD"
-                eval "$CMD"
-            else
-                echo "Skipping $DEST"
+    SOURCE="$x"
+    DEST="$HOME/$(echo ${basex} | sed 's:^config/:.config/:')"
+    if [ -e "$DEST" ] ; then
+        if [ -L "$DEST" ]; then
+            REALSOURCE=`realpath "${SOURCE}"`
+            REALDEST=`readlink "$DEST"`
+            if  [ "$REALSOURCE" == "$REALDEST" ] ; then
+                echo "'${SOURCE}' already installed to '${DEST}'"
                 continue
             fi
         fi
+        if [ "$1" == "-d" ]; then
+            RESPONSE="d"
+        elif [ "$1" == '-b' ]; then
+            RESPONSE="b"
+        elif [ "$1" == '-n' ]; then
+            RESPONSE="n"
+        else
+            read -p "'$DEST' already exists, [d]elete it, [b]ackup, or [n]o change (default) > " RESPONSE
+        fi
+        if [ "$RESPONSE" == "d" ]; then
+            CMD="rm -rf '$DEST'"
+            echo "$CMD"
+            eval "$CMD"
+        elif [ "$RESPONSE" == "b" ]; then
+            CMD="mv -f '$DEST' '$DEST.bak'"
+            echo "$CMD"
+            eval "$CMD"
+        else
+            echo "Skipping '$DEST'"
+            continue
+        fi
     fi
-    CMD="ln -s $SOURCE $DEST"
-    echo "Installing link: $SOURCE -> $DEST"
+    CMD="ln -s '$SOURCE' '$DEST'"
+    echo "Installing link: '$SOURCE' -> '$DEST'"
     eval "$CMD"
 done
